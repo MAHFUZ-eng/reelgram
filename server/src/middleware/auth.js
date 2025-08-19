ce94e08e7caaeff86b29c996365802ba23423df3
Your Name <your@email.com>
2025-08-20 00:29:03 +0600

    Rename user.js to User.js and fix imports

--- server/src/middleware/auth.js
+++ server/src/middleware/auth.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 // Make sure the filename matches exactly: user.js
-const User = require('../../models/user');  
+const User = require('../../models/User');  
 
 const auth = async (req, res, next) => {
   try {
