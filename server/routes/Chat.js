22ec0357560c2a9b39fb8a803f8e1dc1f29abc13
Your Name <your@email.com>
2025-08-20 00:47:50 +0600

    Fix user model import paths to match lowercase filename

--- server/routes/Chat.js
+++ server/routes/Chat.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { auth } = require('../src/middleware/Auth');
 const Message = require('../models/message');
-const User = require('../models/user');
+const User = require('../models/User');
 
 const router = express.Router();
 
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,7 +3,7 @@ const express = require("express");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const crypto = require("crypto");
-const User = require("../models/user");
+const User = require("../models/User");
 const { sendVerificationEmail } = require("../src/services/emailService");
 
 const router = express.Router();
